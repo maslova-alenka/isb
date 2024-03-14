c1e7e81f693469d3016dd0939199c65e9a5c7f20
Алена Маслова <alena09062004@gmail.com>
2024-03-14 22:09:21 +0400

    A frequency analysis was performed, the key from the frequency analysis was compared with the standard one

--- lab_1/task/task_2.py
+++ lab_1/task/task_2.py
@@ -1,5 +1,6 @@
 import os
 from working_with_a_file import*
+from task_1 import decryption
 
 
 def frequency_analysis(text_path: str, path: str) -> None:
@@ -21,6 +22,23 @@ def frequency_analysis(text_path: str, path: str) -> None:
     write_json(sorted_freq, path)
 
 
+def combine_dicts(path1, path2, path_new):
+    dict1 = read_json(path1)
+    dict2 = read_json(path2)
+    keys = dict1.keys()
+    values = dict2.keys()
+    new_dict = {key: value for key, value in zip(keys, values)}
+    write_json(new_dict, path_new)
+
+
 if __name__ == "__main__":
     frequency_analysis(os.path.join('text', 'second_task', 'text_second.txt'),
                        os.path.join('text', 'second_task', 'freq.json'))
+
+    combine_dicts(os.path.join('text', 'second_task', 'freq.json'),
+                  os.path.join('text', 'second_task', 'standard.json'),
+                  os.path.join('text', 'second_task', 'key.json'))
+
+    decryption(os.path.join('text', 'second_task', 'text_second.txt'),
+               os.path.join('text', 'second_task', 'key.json'),
+               os.path.join('text', 'second_task', 'text_decryption.txt'))
