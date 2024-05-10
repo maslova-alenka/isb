6ef09c5b7f629b17656d159c5e7031dcf355587e
Алена Маслова <alena09062004@gmail.com>
2024-05-11 00:11:13 +0400

    add button

--- lab_4/lab_4/main_window.py
+++ lab_4/lab_4/main_window.py
@@ -2,11 +2,11 @@ import sys
 import functions
 import time
 
-from PyQt5.QtCore import QBasicTimer, Qt
+from PyQt5.QtCore import QBasicTimer, Qt, QSize, QEvent
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import (QApplication, QDesktopWidget, QGridLayout, QFormLayout, QLabel, QHBoxLayout,
                              QMainWindow, QMessageBox, QLineEdit, QProgressBar, QPushButton, QSlider,
-                             QVBoxLayout, QWidget)
+                             QVBoxLayout, QWidget, QAction)
 
 
 class MainWindow(QMainWindow):
@@ -14,27 +14,35 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.initUI()
 
-
     def initUI(self):
-        self.resize(1200, 900)
+        self.resize(500, 300)
         self.setWindowTitle('SEARCH OF BANK CARD')
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
 
-        hbox = QHBoxLayout()
-        hbox.addSpacing(1)
+        hash_btn = QPushButton('Find the card number by hash', self)
+        luhn_btn = QPushButton('Check the number using the Luhn algorithm', self)
+        graph_btn = QPushButton('Build a graph', self)
+        exit_btn = QPushButton('Exit', self)
 
-        vbox = QVBoxLayout()
-        vbox.addSpacing(1)
-        vbox.addLayout(hbox)
+        btn_bins = QLineEdit(placeholderText="Enter the list of bins")
+        btn_hash_card = QLineEdit(placeholderText="Enter the hash")
+        btn_last_number = QLineEdit(placeholderText="Enter the last 4 digits")
 
-        self.centralWidget.setLayout(vbox)
+        hbox = QVBoxLayout()
+        hbox.addWidget(hash_btn)
+        hbox.addWidget(luhn_btn)
+        hbox.addWidget(graph_btn)
+        hbox.addWidget(btn_bins)
+        hbox.addWidget(btn_hash_card)
+        hbox.addWidget(btn_last_number)
+        hbox.addWidget(exit_btn)
 
-
+        self.centralWidget.setLayout(hbox)
 
         self.show()
 
-    def closeEvent(self, event):
+    def close_event(self, event):
         reply = QMessageBox.question(self, 'Message', "Are you sure to quit?",
                                      QMessageBox.Yes | QMessageBox.No)
 
