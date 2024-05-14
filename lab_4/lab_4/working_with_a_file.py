06e82dc7db3444d22f8536a42f0de09dfa15bb07
Алена Маслова <alena09062004@gmail.com>
2024-05-14 21:39:49 +0400

    fix bugs

--- lab_4/lab_4/working_with_a_file.py
+++ lab_4/lab_4/working_with_a_file.py
@@ -15,7 +15,7 @@ def read_json(path: str) -> dict:
             data = json.load(file)
         return data
     except FileNotFoundError:
-        print("The file was not found")
+        print(f"The file '{path}' was not found.")
     except Exception as e:
         print(f"An error occurred while reading the JSON file: {str(e)}")
 
@@ -32,6 +32,6 @@ def write_file(path: str, data: list) -> None:
         with open(path, "w", encoding="UTF-8") as file:
             json.dump({"card_number":  data}, file)
     except FileNotFoundError:
-        print("The file was not found")
+        print(f"The file '{path}' was not found.")
     except Exception as e:
         print(f"An error occurred while reading the JSON file: {str(e)}")
